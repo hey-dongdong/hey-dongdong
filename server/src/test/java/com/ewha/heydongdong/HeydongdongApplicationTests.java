259d5cdf11bed3975f6d21e49264d78add42c876
jeongwon-iee <garden1117244@gmail.com>
2020-10-08 14:50:05 +0900

    [Server] Initial Settings

---
+++ server/src/test/java/com/ewha/heydongdong/HeydongdongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewha.heydongdong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeydongdongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
