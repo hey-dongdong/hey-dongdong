d64eb71aff308eb10f9aafd2ac8b78ee2436b427
sdsdsrd <1jmss@naver.com>
2020-10-17 05:15:34 +0900

    [Client] Update SignUpPage parameter name

--- client/customer/src/views/SignUpPage.vue
+++ client/customer/src/views/SignUpPage.vue
@@ -5,18 +5,18 @@
 		</AppHeader>
 		<div class="greenbg">
 			<form @submit.prevent="submitForm" class="form">
-				<label for="id" class="form-label">ID</label>
-				<input id="id" type="text" v-model="id" />
+				<label for="userId" class="form-label">ID</label>
+				<input id="userId" type="text" v-model="id" />
 				<label for="password" class="form-label">Password</label>
 				<input id="password" type="password" v-model="password" />
 				<label for="confirmpw" class="form-label">Password 확인</label>
 				<input id="confirmpw" type="password" />
-				<label for="name" class="form-label">이름</label>
-				<input id="name" type="text" v-model="name" />
+				<label for="username" class="form-label">이름</label>
+				<input id="username" type="text" v-model="name" />
 				<label for="email" class="form-label">이메일</label>
 				<input id="email" type="email" v-model="email" />
-				<label for="phonenumber" class="form-label">휴대폰번호</label>
-				<input id="phonenumber" type="tel" v-model="phonenumber" />
+				<label for="phone" class="form-label">휴대폰번호</label>
+				<input id="phone" type="tel" v-model="phonenumber" />
 				<button type="submit" class="goldbtn">가입하기</button>
 			</form>
 		</div>
@@ -32,9 +32,9 @@ export default {
 	},
 	data() {
 		return {
-			id: '',
+			userId: '',
 			password: '',
-			name: '',
+			username: '',
 			email: '',
 			phonenumber: '',
 		};
