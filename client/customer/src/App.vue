60973b7eeb69bcca93aef079dbd6d0fa3907690c
sdsdsrd <1jmss@naver.com>
2020-10-17 02:52:00 +0900

    [Client] Add common.css

--- client/customer/src/App.vue
+++ client/customer/src/App.vue
@@ -1,31 +1,12 @@
 <template>
 	<div id="app">
-		<img alt="Vue logo" src="./assets/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js App" />
+		안녕
 	</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue';
-
-export default {
-	name: 'App',
-	components: {
-		HelloWorld,
-	},
-	created() {
-		console.log('hi');
-	},
-};
-</script>
+<script></script>
 
 <style>
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-}
+@import './css/reset.css';
+@import './css/common.css';
 </style>
