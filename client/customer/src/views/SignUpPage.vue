669759b6272b38d67ee0424b1a4fba8c819e4ccb
sdsdsrd <1jmss@naver.com>
2020-10-17 05:11:14 +0900

    [Client] Add SignUpPage data binding

--- client/customer/src/views/SignUpPage.vue
+++ client/customer/src/views/SignUpPage.vue
@@ -6,17 +6,17 @@
 		<div class="greenbg">
 			<form @submit.prevent="submitForm" class="form">
 				<label for="id" class="form-label">ID</label>
-				<input id="id" type="text" />
+				<input id="id" type="text" v-model="id" />
 				<label for="password" class="form-label">Password</label>
-				<input id="password" type="password" />
+				<input id="password" type="password" v-model="password" />
 				<label for="confirmpw" class="form-label">Password 확인</label>
 				<input id="confirmpw" type="password" />
 				<label for="name" class="form-label">이름</label>
-				<input id="name" type="text" />
+				<input id="name" type="text" v-model="name" />
 				<label for="email" class="form-label">이메일</label>
-				<input id="email" type="email" />
+				<input id="email" type="email" v-model="email" />
 				<label for="phonenumber" class="form-label">휴대폰번호</label>
-				<input id="phonenumber" type="number" />
+				<input id="phonenumber" type="tel" v-model="phonenumber" />
 				<button type="submit" class="goldbtn">가입하기</button>
 			</form>
 		</div>
@@ -38,6 +38,11 @@ export default {
 			email: '',
 			phonenumber: '',
 		};
+	},
+	methods: {
+		submitForm() {
+			console.log('폼 제출');
+		},
 	},
 };
 </script>
