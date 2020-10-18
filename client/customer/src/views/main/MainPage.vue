9f867ee2ab078f242bdda65f34354a14363a3292
sdsdsrd <1jmss@naver.com>
2020-10-19 06:15:43 +0900

    [Client] Update MainPage - Add advertisements

--- client/customer/src/views/main/MainPage.vue
+++ client/customer/src/views/main/MainPage.vue
@@ -1,32 +1,42 @@
 <template>
-	<div class="blackbg">
+	<div class="main">
 		<MainHeader></MainHeader>
-		<PickUpStore></PickUpStore>
-		<div class="progress-container">
-			<img src="../../assets/progress50.png" alt="progress" class="progress" />
-			<img src="../../assets/logo.png" alt="logo" class="small-logo" />
-			<div class="progress-percent">
-				<p class="percent">50</p>
-				<p class="total-percent">/100%</p>
+		<div class="blackbg-main">
+			<PickUpStore></PickUpStore>
+			<div class="progress-container">
+				<img
+					src="../../assets/progress50.png"
+					alt="progress"
+					class="progress"
+				/>
+				<img src="../../assets/logo.png" alt="logo" class="small-logo" />
+				<div class="progress-percent">
+					<p class="percent">50</p>
+					<p class="total-percent">/100%</p>
+				</div>
+				<p class="progress-explain">주문 확인 중입니다</p>
 			</div>
-			<p class="progress-explain">주문 확인 중입니다</p>
+			<img src="../../assets/barcode.png" alt="barcode" class="barcode" />
+			<ul class="linkboxes">
+				<li class="page-link">
+					<router-link class="link-name" to="/menu/all"
+						>전체<br />메뉴</router-link
+					>
+				</li>
+				<li class="page-link">
+					<router-link class="link-name" to="/my-menu"
+						>나만의<br />메뉴</router-link
+					>
+				</li>
+				<li class="page-link">
+					<router-link class="link-name" to="/history">히스토리</router-link>
+				</li>
+			</ul>
+			<img src="../../assets/ad-1.png" alt="ad" class="ad" />
+			<img src="../../assets/ad-2.png" alt="ad" class="ad" />
+			<img src="../../assets/ad-3.png" alt="ad" class="ad" />
+			<img src="../../assets/ad-4.png" alt="ad" class="ad" />
 		</div>
-		<img src="../../assets/barcode.png" alt="barcode" class="barcode" />
-		<ul class="linkboxes">
-			<li class="page-link">
-				<router-link class="link-name" to="/menu/all"
-					>전체<br />메뉴</router-link
-				>
-			</li>
-			<li class="page-link">
-				<router-link class="link-name" to="/my-menu"
-					>나만의<br />메뉴</router-link
-				>
-			</li>
-			<li class="page-link">
-				<router-link class="link-name" to="/history">히스토리</router-link>
-			</li>
-		</ul>
 	</div>
 </template>
 
