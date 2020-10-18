6bfb9022f5a88c93c49cba4720b69ea8847e035d
sdsdsrd <1jmss@naver.com>
2020-10-18 22:40:22 +0900

    [Client] Add FindIdPage

--- client/customer/src/views/FindIdPage.vue
+++ client/customer/src/views/FindIdPage.vue
@@ -7,7 +7,7 @@
 			<form @submit.prevent="submitForm" class="form">
 				<label for="username" class="form-label">이름</label>
 				<input id="username" type="text" placeholder="이름을 입력하세요" />
-				<label for="email" class="form-label">Password</label>
+				<label for="email" class="form-label">이메일</label>
 				<input id="email" type="email" placeholder="이메일을 입력하세요" />
 				<button type="submit" class="goldbtn">확인</button>
 			</form>
