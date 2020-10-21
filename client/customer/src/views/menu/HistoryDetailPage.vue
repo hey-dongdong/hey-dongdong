72fa1a5ea403e9c4ca1d5cac7cc6ebc0b18ccd86
sdsdsrd <1jmss@naver.com>
2020-10-21 19:19:54 +0900

    [Client] Update HistoryDetailPage

--- client/customer/src/views/menu/HistoryDetailPage.vue
+++ client/customer/src/views/menu/HistoryDetailPage.vue
@@ -6,6 +6,10 @@
 		<div class="blackbg">
 			<OrderDetail></OrderDetail>
 			<OrderItems></OrderItems>
+			<div class="greenbtn-small-set">
+				<button type="button" class="greenbtn-small">이대로 주문하기</button>
+				<button type="button" class="greenbtn-small">장바구니에 담기</button>
+			</div>
 		</div>
 	</div>
 </template>
