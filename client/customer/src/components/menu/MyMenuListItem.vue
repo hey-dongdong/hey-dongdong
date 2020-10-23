1a6caf9ed22c44c09c0f5f16b535e42bd1d3da0c
sdsdsrd <1jmss@naver.com>
2020-10-23 21:58:24 +0900

    [Client] Update MyMenuDetailPage

--- client/customer/src/components/menu/MyMenuListItem.vue
+++ client/customer/src/components/menu/MyMenuListItem.vue
@@ -1,22 +1,25 @@
 <template>
-	<div class="menu-card" @click="goToMyMenuDetail(myMenuItem)">
-		<img
-			:src="
-				myMenuItem.menuInOrder.menu.imgUrl
-					? require('@/assets' + myMenuItem.menuInOrder.menu.imgUrl)
-					: require('@/assets/cappuccino.png')
-			"
-			alt="메뉴이미지"
-			class="menu-img"
-		/>
-		<div class="menu-text">
-			<span class="menu-name">{{ myMenuItem.menuInOrder.menu.menuName }}</span>
-			<span class="menu-detail">
-				<span v-if="myMenuItem.menuInOrder.option.basicOption.isHot">HOT </span>
-				<span v-if="!myMenuItem.menuInOrder.option.basicOption.isHot">ICE </span>
-				<span>{{ myMenuItem.menuInOrder.price }}원</span>
-			</span>
+	<div class="menu-card">
+		<div class="menu-card-without-x" @click="goToMyMenuDetail(myMenuItem)">
+			<img
+				:src="
+					myMenuItem.menuInOrder.menu.imgUrl
+						? require('@/assets' + myMenuItem.menuInOrder.menu.imgUrl)
+						: require('@/assets/cappuccino.png')
+				"
+				alt="메뉴이미지"
+				class="menu-img"
+			/>
+			<div class="menu-text">
+				<span class="menu-name">{{ myMenuItem.menuInOrder.menu.menuName }}</span>
+				<span class="menu-detail">
+					<span v-if="myMenuItem.menuInOrder.option.basicOption.isHot">HOT </span>
+					<span v-if="!myMenuItem.menuInOrder.option.basicOption.isHot">ICE </span>
+					<span>{{ myMenuItem.menuInOrder.price }}원</span>
+				</span>
+			</div>
 		</div>
+
 		<slot name="delete-button"></slot>
 	</div>
 </template>
