9426ac596fe385cfe18af4f8e0b9712879f720da
jiwoo-kimm <jiwoo.kim.dev@gmail.com>
2020-10-28 15:48:27 +0900

    [Server] Add basic MenuInOrder, Option functions & tests

---
+++ server/src/main/java/com/ewha/heydongdong/repository/MenuInOrderRepository.java
@@ -0,0 +1,7 @@
+package com.ewha.heydongdong.repository;
+
+import com.ewha.heydongdong.domain.MenuInOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuInOrderRepository extends JpaRepository<MenuInOrder, Long> {
+}
--- server/src/main/java/com/ewha/heydongdong/repository/OrderRepository.java
+++ server/src/main/java/com/ewha/heydongdong/repository/OrderRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, String> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
     List<Order> findByUser(User user);
 }
