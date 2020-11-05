5f652eecee546ef2f885c5bc8fa24d7f386dbcc7
jeongwon-iee <garden1117244@gmail.com>
2020-11-05 18:37:58 +0900

    [Server] Add WebSocket

---
+++ server/src/main/java/com/ewha/heydongdong/module/service/OrderService.java
@@ -0,0 +1,9 @@
+package com.ewha.heydongdong.module.service;
+
+import com.ewha.heydongdong.module.repository.OrderRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+    OrderRepository orderRepository;
+}
