c084ef7e3db01e334a86ac4e96a5acf635a94b25
sdsdsrd <1jmss@naver.com>
2020-11-08 11:19:22 +0900

    [Client] Update api index.js and css

--- client/administrator/src/api/index.js
+++ client/administrator/src/api/index.js
@@ -1,1 +1,17 @@
-// import axios from 'axios';
+import axios from 'axios';
+
+function createInstance() {
+	const instance = axios.create();
+	return instance;
+}
+const instance = createInstance();
+
+async function fetchHistoryMenus(id, data) {
+	try {
+		return await instance.post(`/admin/history/${id}`, data);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+export { fetchHistoryMenus };
