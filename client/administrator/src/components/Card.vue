f44c05b4528b3d41acf461e3e3b6b1f58213d407
sdsdsrd <1jmss@naver.com>
2020-11-09 17:35:25 +0900

    [Client] Add ButtonBox component

--- client/administrator/src/components/Card.vue
+++ client/administrator/src/components/Card.vue
@@ -8,24 +8,24 @@
 					:menuItem="menuItem"
 				></MenuInCard>
 			</div>
-			<span class="time">16:04, 9/7 월, 2020</span>
+			<span class="time">{{ orderItem.orderInfo.orderAt }}</span>
 		</div>
-
-		<div class="button-container">
-			<button type="button" class="accept">
+		<ButtonBox>
+			<button slot="button1" type="button" class="accept">
 				<span>수락</span>
 				<ion-icon name="checkmark-circle" class="check"></ion-icon>
 			</button>
-			<button type="button">
+			<button slot="button2" type="button">
 				<ion-icon name="close-circle" class="x"></ion-icon>
 				<span>거절</span>
 			</button>
-		</div>
+		</ButtonBox>
 	</div>
 </template>
 
 <script>
 import MenuInCard from './MenuInCard.vue';
+import ButtonBox from './ButtonBox.vue';
 
 export default {
 	name: 'order-item',
@@ -34,6 +34,7 @@ export default {
 	},
 	components: {
 		MenuInCard,
+		ButtonBox,
 	},
 };
 </script>
