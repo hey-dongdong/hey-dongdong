03379392c9ce0b892c7510112403f4d9365e1383
sdsdsrd <1jmss@naver.com>
2020-11-10 05:13:17 +0900

    [Client] Update according to Refactoring api

--- client/customer/src/store/actions.js
+++ client/customer/src/store/actions.js
@@ -13,7 +13,7 @@ export default {
 	},
 	async FETCH_HISTORY({ commit }, data) {
 		const response = await fetchHistory(data);
-		commit('SET_HISTORY', response.data.payload.orders);
+		commit('SET_HISTORY', response.data.payload.menus);
 		return response;
 	},
 	async FETCH_HISTORY_DETAIL({ commit }, data) {
