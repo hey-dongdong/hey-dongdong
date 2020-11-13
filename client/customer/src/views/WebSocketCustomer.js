df0a2a1d52a1de0cff36569916e058c998da76db
jeongwon-iee <garden1117244@gmail.com>
2020-11-13 20:27:19 +0900

    [Client] Add WebSocketAdmin & Customer

---
+++ client/customer/src/views/WebSocketCustomer.js
@@ -0,0 +1,68 @@
+const MessageType = {
+    MESSAGE: 'message'
+};
+
+export default class WebSocketAdmin {
+    connect() {
+        this.socket = new WebSocket('ws://localhost:8082');
+        this.onOpen = this.onOpen.bind(this); //TODO 연결이 수립되면 서버에 메시지 전송
+        this.onMessage = this.onMessage.bind(this);
+        this.socket.addEventListener('open', this.onOpen);
+        this.socket.addEventListener('message', this.onMessage);
+        this._callbacks = {};
+    }
+    onOpen () {
+        this.doCallback('open');
+    }
+    on (event, callback) {
+        this._callbacks[event] = this._callbacks[event] || [];
+        this._callbacks[event].push(callback);
+    }
+    isOpen () {
+        return this.socket.readyState === this.socket.OPEN
+    }
+    doCallback (event, data) {
+        const callbacks = this._callbacks[event];
+        if (callbacks) {
+            callbacks.map(cb => cb(data));
+        }
+    }
+    status () {
+        switch (this.socket.readyState) {
+            case this.socket.OPEN:
+                return 'open';
+            case this.socket.CLOSED:
+                return 'closed';
+            case this.socket.CLOSING:
+                return 'closing';
+        }
+    }
+    onMessage (event) {
+        try {
+            const parsedMessage = JSON.parse(event.data);
+            console.info(event.data);
+            console.info(parsedMessage);
+            console.info(parsedMessage.type);
+            console.info(parsedMessage.payload);
+            switch(parsedMessage.type) {
+                case MessageType.MESSAGE:
+                    console.log(`Message from admin: ${parsedMessage.payload}`);
+                    break;
+                default:
+                    console.log(`Message from admin: ${parsedMessage.payload}`);
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    sendData (command, payload) {
+        const data = {
+            command,
+            data: btoa(payload)
+        };
+
+        if(!this.isOpen()) return;
+        this.socket.send(JSON.stringify(data));
+    }
+}
