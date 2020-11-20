4e940921ecabfa8119676ef833d5868171a48ac4
jiwoo-kimm <jiwoo.kim.dev@gmail.com>
2020-11-21 03:35:17 +0900

    [Server] Add CORS Config

---
+++ server/src/main/java/com/ewha/heydongdong/infra/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.ewha.heydongdong.infra.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://13.209.112.163:8080");
+
+    }
+}
