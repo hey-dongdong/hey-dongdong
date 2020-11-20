7155e7d5d14027e37162c913f6dd80f3c33f8dfb
jiwoo-kimm <jiwoo.kim.dev@gmail.com>
2020-11-21 03:41:13 +0900

    [Server] Update CORS Config

--- server/src/main/java/com/ewha/heydongdong/infra/config/WebMvcConfig.java
+++ server/src/main/java/com/ewha/heydongdong/infra/config/WebMvcConfig.java
@@ -10,7 +10,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://13.209.112.163:8080");
+                .allowedOrigins("http://13.209.112.163:8080", "http://13.209.112.163:8080/**", "*")
+                .allowedMethods("GET", "PUT", "POST");
 
     }
 }
